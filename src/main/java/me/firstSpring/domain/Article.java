788965695694f6a9da32adb5cf05b611cdf2f937
Kim-seng-won <rlatmddnjs0103@naver.com>
2023-08-02 22:33:36 +0900

    test2

--- src/main/java/me/firstSpring/domain/Article.java
+++ src/main/java/me/firstSpring/domain/Article.java
@@ -29,17 +29,6 @@ public class Article {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @Builder
-    public Article(String author, String title, String content) {
-        this.author = author;
-        this.title = title;
-        this.content = content;
-    }
-
-    public void update(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
     @CreatedDate //엔터티가 생성될 때 생성 시간 저장
     @Column(name = "created_at")
     private LocalDateTime createAt;
@@ -47,4 +36,19 @@ public class Article {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updateAt;
+
+    @Builder
+    public Article(String author, String title, String content, LocalDateTime createAt, LocalDateTime updateAt) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
 }
