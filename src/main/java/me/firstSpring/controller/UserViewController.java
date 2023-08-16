61d331c272c3f2b96a60d1b11eae9a653beb700b
Kim-seng-won <rlatmddnjs0103@naver.com>
2023-08-16 23:12:57 +0900

    0816

--- src/main/java/me/firstSpring/controller/UserViewController.java
+++ src/main/java/me/firstSpring/controller/UserViewController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.firstSpring.config.jwt.TokenProvider;
 import me.firstSpring.domain.User;
 import me.firstSpring.service.UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,9 +31,9 @@ public class UserViewController {
     @GetMapping("sign-up")
     public String showUserProfile(Authentication authentication, Model model) {
         if (authentication != null && authentication.isAuthenticated()) {
-            String userEmail = authentication.getName();
-            User user = userService.findByEmail(userEmail);
-            model.addAttribute("user", user);
+            OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+            User user = userService.findByEmail((String) oAuth2User.getAttributes().get("email"));
+            model.addAttribute("users", user);
             return "signup"; // user/profile.html 템플릿을 렌더링합니다.
         } else {
             // 인증되지 않은 상태에 대한 처리
