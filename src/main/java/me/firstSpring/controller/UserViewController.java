cfa985144e3baa4a171803dedb5a363e22265aba
Kim-seng-won <rlatmddnjs0103@naver.com>
2023-08-17 00:51:04 +0900

    0817

--- src/main/java/me/firstSpring/controller/UserViewController.java
+++ src/main/java/me/firstSpring/controller/UserViewController.java
@@ -1,12 +1,20 @@
 package me.firstSpring.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.firstSpring.config.jwt.TokenProvider;
+import me.firstSpring.domain.User;
+import me.firstSpring.service.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class UserViewController {
+
+    private UserService userService;
+    private TokenProvider tokenProvider;
     @GetMapping("/login")
     public String login() {
         return "oauthLogin";
@@ -14,6 +22,19 @@ public class UserViewController {
 
     @GetMapping("/signup")
     public String signup() {
+        return "signup";
+    }
+
+    @GetMapping("/sign-up")
+    public String newUsers(Authentication authentication, Model model){
+        if (authentication != null && authentication.isAuthenticated()) {
+            String tokenValue = (String) authentication.getCredentials();
+
+            Long userId = tokenProvider.getUserId(tokenValue);
+
+            User user = userService.findById(userId);
+            model.addAttribute("user", user);
+        }
         return "signup";
     }
 }
