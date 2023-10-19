ab20a1fcedfe5819665b7c67f8d93ba4929c6f38
Kim-seng-won <rlatmddnjs0103@naver.com>
2023-10-19 13:33:05 +0900

    1019

--- out/production/resources/static/js/upload.js
+++ out/production/resources/static/js/upload.js
@@ -1,38 +1,28 @@
-// More API functions here:
-// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
-
-// the link to your model provided by Teachable Machine export panel
-const modelPATH = "/js/model/";
-
-const modelURL = modelPATH + "model.json";
-const metadataURL = modelPATH + "metadata.json";
+const img = document.getElementById('img');
 const result = document.getElementById('result');
+let input = document.getElementById('input');
+const modelPath = "/js/model/";
+const modelURL = modelPath + "model.json";
+const metadataURL = modelPath + "metadata.json";
 
-function loadFile(input) {
-    var file = input.files[0];
+tmImage.load(modelURL, metadataURL).then(model => {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('status').innerHTML = "나의 모델 로딩 완료"
 
-    newImage = document.getElementById('show-img')
+    function run() {
+        model.predict(img).then(predictions => {
+            console.log('Predictions: ', predictions);
+            predictions.sort((a, b) => (b.probability - a.probability));
+            result.innerHTML = predictions[0].className + ' ' + parseInt(predictions[0].probability * 100) + '%';
+        });
+    }
 
-    newImage.src = modelPATH.createObjectURL(file);
+    input.addEventListener('change', (e) => {
+        img.src = URL.createObjectURL(e.target.files[0]);
+    }, false);
 
-    newImage.style.width = "300px";
-    newImage.style.height = "300px";
-    newImage.style.objectFit = "contain";
+    img.onload = function () {
+        run();
+    };
 
-    var container = document.getElementById('image-show');
-    container.appendChild(newImage);
-
-    const img = document.getElementById('show-img');
-    tmImage.load(modelURL, metadataURL).then(model => {
-        function run(){
-            model.predict(img).then(predictions => {
-                console.log('Predictions: ', predictions);
-                predictions.sort((a, b) => (b.probability - a.probability)); //내림차순으로 정렬 오름차순이면 a-b
-                result.innerHTML = predictions[0].className + ' ' + parseInt(predictions[0].probability * 100) + '%';
-            });
-        }
-        img.onload = function () {
-             run();
-        };
-    });
-};
+});
